740cfe8beadc66499e229e21e3575377c2552dfc
Laura-Cuesta <97395621+Laura-Cuesta@users.noreply.github.com>
2022-05-04 15:01:11 +0200

    Add files via upload

---
+++ app_model_ejer.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import os
+import pickle
+
+os.chdir(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+model = pickle.load(open('model/ad_model.pkl','rb'))# leemos el modelo
+
+@app.route('/api/v1/predict', methods=['GET'])# creamos la ruta api para obtener los dtos
+def predict():
+    
+    tv = request.args.get('tv', None)# cogete tv, si no está ponle none
+    radio = request.args.get('radio', None)# cogete radio, si no está ponle none
+    newspaper = request.args.get('newspaper', None)# cogete newspapel, si no está ponle none
+
+    if tv is None or radio is None or newspaper is None:
+        return "Args empty, the data are not enough to predict"
+    else:
+        prediction = model.predict([[tv,radio,newspaper]])
+    
+    return jsonify({'predictions': prediction[0]})
+
+app.run()
